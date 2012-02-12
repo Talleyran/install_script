6cd31740c009cad68acd49871eaa3e47600ad1c1
Kirill Jakovlev <special-k@li.ru>
2012-02-12 20:34:33 +0400

    change github config way

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,4 @@
-##!/bin/zsh
+#!/bin/zsh
 ruby='1.9.3'
 mapserver='mapserver-6.0.2'
 pgpass='514790'
@@ -10,35 +10,36 @@ cd ~
 
 #clean
 rm -rf .vim
-rm .vimrc
+rm -f .vimrc
 rm -rf .oh-my-zsh
-rm .zshrc
+rm -f .zshrc
 rm -rf source/$mapserver
 rm -f source/$mapserver.tar.gz
+rm -rf .rvm
+
+#sudo
+sudo echo Now sudo
+
+#set shell
+chsh -s /bin/zsh
+
+#gen rsa
+ssh-keygen -t rsa
 
 # update & upgrade
 sudo apt-get update
 sudo apt-get dist-upgrade
 
 #system
-ssh-keygen -t rsa
-sudo apt-get install zsh curl git-core
+sudo apt-get install curl git-core
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-https://raw.github.com/Talleyran/install_script/master/files/.vimrc
 curl https://raw.github.com/Talleyran/install_script/master/files/special_dallas.zsh-theme > .oh-my-zsh/themes/special_dallas.zsh-theme
 curl https://raw.github.com/Talleyran/install_script/master/files/.zshrc > .zshrc
 
 #git
-git config --global user.name $name
-git config --global user.email $email
-git config --global github.user $githubuser
-
-#ruby
-sudo apt-get install build-essential libssl-dev libreadline-dev zlib1g-dev libssl-dev libpq-dev libxml2 libxml2-dev libxslt-dev libmagickwand-dev
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
-source ~/.zshrc
-rvm install $ruby
-rvm use $ruby --default
+git config --global --replace-all user.name "$name"
+git config --global --replace-all user.email "$email"
+git config --global --replace-all github.user "$githubuser"
 
 #vim
 git clone git@github.com:Talleyran/myvim.git ~/.vim
@@ -49,6 +50,13 @@ cp ~/.vim/.vimrc ~/
 cp ~/.vim/.iabbrev ~/
 ln -s ~/.vim/vim-pathogen/autoload ~/.vim/autoload
 cd ~
+
+#ruby
+sudo apt-get install build-essential libssl-dev libreadline-dev zlib1g-dev libssl-dev libpq-dev libxml2 libxml2-dev libxslt-dev libmagickwand-dev
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+source ~/.zshrc
+rvm install $ruby
+rvm use $ruby --default
 
 #desktop
 sudo apt-get install xclip
@@ -114,7 +122,8 @@ do
 done
 cd ~
 
-
-
 #autoclean
 sudo apt-get autoclean
+
+#after all
+source ~/.zshrc
