21758d4af72b93ded7c83f4ebad666f584c75a74
Kirill Jakovlev <special-k@li.ru>
2012-02-12 18:35:12 +0400

    init

---
+++ files/.zshrc
@@ -0,0 +1,60 @@
+# Path to your oh-my-zsh configuration.
+export ZSH=$HOME/.oh-my-zsh
+
+# Set to the name theme to load.
+# Look in ~/.oh-my-zsh/themes/
+export ZSH_THEME="special_dallas"
+
+# Set to this to use case-sensitive completion
+# export CASE_SENSITIVE="true"
+
+# Comment this out to disable weekly auto-update checks
+# export DISABLE_AUTO_UPDATE="true"
+
+# Uncomment following line if you want to disable colors in ls
+# export DISABLE_LS_COLORS="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(rake git mercurial)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize to your needs...
+alias addppa="sudo add-apt-repository"
+alias aptup="sudo apt-get update"
+alias aptgr="sudo apt-get upgrade"
+alias aptsgr="sudo apt-get dist-upgrade"
+alias apti="sudo apt-get install"
+
+alias r="rails"
+
+alias gsvim='gvim --servername onevim'
+alias gsvimadd='gsvim --remote'
+
+alias unmdb='sudo rm /var/lib/mongodb/mongod.lock'
+alias rgs='sudo service geoserver start'
+alias stgs='sudo service geoserver stop'
+alias regs='sudo service geoserver restart'
+alias rmdb='sudo service mongodb start'
+alias stmdb='sudo service mongodb stop'
+alias remdb='sudo service mongodb restart'
+
+alias ry='rsync --progress --delete-after'
+
+#git
+alias gg='git log --graph --date-order -C -M --pretty=format:"<%h> %ad [%an] %Cgreen%d%Creset %s" --all --date=short'
+alias ggg='git log --walk-reflogs'
+alias gggg='gitk --all $(git log --walk-reflogs --pretty=format:%h)'
+alias gr='git rebase'
+alias gri='git rebase -i'
+alias grc='git rebase --continue'
+alias grs='git reset HEAD'
+alias grsh='git reset --hard'
+alias gcur='git rev-parse HEAD'
+alias gcam='git commit --amend'
+alias gar="git add .&&git ls-files --deleted | xargs git rm"
+
+alias xclip='xclip -selection c'
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
---
+++ install-all.sh
@@ -0,0 +1,112 @@
+##!/bin/zsh
+ruby=1.9.3
+mapserver=mapserver-6.0.2
+pgpass=514790
+name=Kirill Jakovlev
+email=special-k@li.ru
+githubuser=$USER
+
+cd ~
+
+# update & upgrade
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+
+#system
+ssh-keygen -t rsa
+sudo apt-get install zsh curl git-core
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
+curl https://raw.github.com/gist/1801369/616aed10ae2168f12e3341ae95451a3e3f80ce75/special_dallas.zsh-theme > .oh-my-zsh/themes/special_dallas.zsh-theme
+curl https://raw.github.com/gist/1802469/2bc8096ee4079083fb6d59e71863ebe3effabc58/.zshrc > .zshrc
+
+#git
+git config --global user.name $name
+git config --global user.email $email
+git config --global github.user $githubuser
+
+#ruby
+sudo apt-get install build-essential libssl-dev libreadline-dev zlib1g-dev libssl-dev libpq-dev libxml2 libxml2-dev libxslt-dev libmagickwand-dev
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+source ~/.zshrc
+rvm install $ruby
+rvm use $ruby --default
+
+#vim
+git clone git@github.com:Talleyran/myvim.git ~/.vim
+cd ~/.vim
+git submodule init
+git submodule update
+cp ~/.vim/.vimrc ~/
+cp ~/.vim/.iabbrev ~/
+ln -s ~/.vim/vim-pathogen/autoload ~/.vim/autoload
+cd ~
+
+#desktop
+sudo apt-get install xclip
+sudo apt-get install vlc
+sudo apt-get purge totem
+sudo apt-get purge banshee
+sudo add-apt-repository ppa:alexey-smirnov/deadbeef
+sudo apt-get update
+sudo apt-get install deadbeef
+
+#postgres
+sudo apt-get install postgresql
+sudo -u postgres createuser -s special-k
+sudo -u postgres psql -c "alter role \"special-k\" password '$pgpass';"
+sudo apt-get install pgadmin3
+
+#gis
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install qgis
+sudo apt-get install mapserver-bin
+
+#mapscript
+sudo apt-get install libfreetype6-dev libgif-dev libpng-dev libjpeg-dev libgdal-dev libgd2-xpm-dev libproj-dev libcurl4-openssl-dev libxslt-dev libghc6-cairo-dev swig
+if [ ! -d ~/source ]; then
+  mkdir ~/source
+fi
+cd ~/source
+curl http://download.osgeo.org/mapserver/$mapserver.tar.gz > $mapserver.tar.gz
+tar xvzf $mapserver.tar.gz
+cd $mapserver
+./configure --with-gdal=/usr/bin/gdal-config \
+--with-ogr=/usr/bin/gdal-config \
+--with-wfsclient \
+--with-wmsclient \
+--with-curl-config=/usr/bin/curl-config \
+--with-proj=/usr/ \
+--with-tiff \
+--with-jpeg \
+--with-freetype=/usr/ \
+--with-threads \
+--with-wcs \
+--with-postgis=yes \
+--with-libiconv=/usr \
+--with-geos=/usr/bin/geos-config \
+--with-xml2-config=/usr/bin/xml2-config \
+--with-sos \
+--without-agg-svg-symbols \
+--with-cairo=yes \
+--with-kml=yes \
+--with-exslt
+make
+cd mapscript/ruby
+ruby ./extconf.rb
+make
+for i in $(ruby -e 'puts $LOAD_PATH')
+do
+  for j in $(find $i -maxdepth 1 -name '*.so')
+  do
+    cp *.so $i/
+    break
+  done
+done
+cd ~
+
+
+
+#autoclean
+sudo apt-get autoclean
